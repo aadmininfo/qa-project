dc9faf0017bce6d6476d4d6a1ee003d163b0b2dc
aadmininfo <lionakimov797@gmail.com>
2025-07-20 19:39:45 +0300

    Deploy: корректные пути

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,18 @@
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/qa-project/", // ?? ЭТО ДОБАВЬ
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
+    // другие плагины
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
